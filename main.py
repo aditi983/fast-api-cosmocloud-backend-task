1c43bf55ff16c7612d39c7b2c16e17b6036d0f32
Aditi <78065249+aditi983@users.noreply.github.com>
2024-04-09 23:00:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from routes import students
 app = FastAPI()
 
 app.include_router(students.router, prefix="/api")
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=True)
